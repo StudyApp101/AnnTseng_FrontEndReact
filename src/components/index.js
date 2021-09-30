6f77dc0337269e97b7327d6692cc3b35b95361e0
Ann-c-Tseng <ann.c.tseng@gmail.com>
2021-09-30 16:17:16 -0700

    reorganized index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,16 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Navbar2 } from './Navbar2/Navbar2';
 export { default as Footer } from './Footer/Footer';
+
 export { default as InfoSection } from './InfoSection/InfoSection';
+export { default as VideoSection } from './VideoSection/VideoSection';
+
+export { default as OurStory } from './OurStory/OurStory';
+export { default as Feedback } from './Feedback/Feedback';
+
 export { default as Signup } from './Signup/Signup';
 export { default as Login } from './Login/Login';
-export { default as OurStory } from './OurStory/OurStory';
+
 export { default as Upload } from './Upload/Upload';
-export { default as VideoSection } from './VideoSection/VideoSection';
-export { default as Feedback } from './Feedback/Feedback';
 
 export { default as Mynotes } from './Mynotes/Mynotes';
