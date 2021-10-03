55ef0fc77cf47a2f0a979f272fe13dcd89b3addc
Ann-c-Tseng <ann.c.tseng@gmail.com>
2021-10-02 23:05:25 -0700

    all functions working for display pdfgit add .

--- src/components/Mynotes/Mynotes.js
+++ src/components/Mynotes/Mynotes.js
@@ -14,27 +14,20 @@ const Mynotes = ({}) => {
     const onInputChange = (e) => {
         pdffile_url=URL.createObjectURL(e.target.files[0])
         console.log(pdffile_url)
-        document.getElementsByTagName('embed')[0].src=pdffile_url
+        document.getElementsByTagName('iframe')[0].src=pdffile_url
     };
     return (
         <>
-            <form>
-                <input id="uploadPDF" onChange={onInputChange} type="file" name="file"/>
-                <div>
-                    <embed src="" frameborder="0" scrolling="yes" width="300" height="200"></embed>
-                </div>
-            </form>
+            <InfoSec>
+                <Container>
+                    <InfoRow>
+                        <input id="uploadPDF" onChange={onInputChange} type="file" name="file" accept=".pdf"/>
+                        <PDFviewer src=""></PDFviewer>
+                        <Noteviewer> Start typing here... </Noteviewer>
+                    </InfoRow>   
+                </Container>
+            </InfoSec> 
         </>
-        // <>
-        //     <InfoSec>
-        //         <Container>
-        //             <InfoRow>
-        //                 <PDFviewer src="https://drive.google.com/file/d/1FkcsLNKRD3qmkC_p_RuV6_fzkzo98Qnh/preview"></PDFviewer>
-        //                 <Noteviewer> Start typing here... </Noteviewer>
-        //             </InfoRow>   
-        //         </Container>
-        //     </InfoSec> 
-        // </>
     );
 };
 
